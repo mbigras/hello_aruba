4ca65210a7c686797cb2380da088db65a2d51c6d
Max Bigras <mbigras22@gmail.com>
2018-03-23 20:48:40 -0700

    Make key insight about how $LOAD_PATH works!
    https://stackoverflow.com/a/4479926/2909897

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,11 +1,10 @@
-LIB_DIR = File.expand_path("../../../lib", __FILE__)
-# $LOAD_PATH.unshift(LIB_DIR)
-
 require 'aruba/cucumber'
+
+LIB = File.expand_path("../../lib", __dir__)
 
 Before do
   @original_rubylib = ENV['RUBYLIB']
-  ENV['RUBYLIB'] = LIB_DIR + File::PATH_SEPARATOR + ENV['RUBYLIB'].to_s
+  ENV['RUBYLIB'] = LIB + File::PATH_SEPARATOR + ENV['RUBYLIB'].to_s
 end
 
 After do
